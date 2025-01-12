a71af985cdedf4b3c33629544d12ec4dda45e767
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-12 15:19:42 +0100

    fix: update routes imports

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
-import * as auth from './auth';
-import * as reservation from './reservation';
-import * as room from './room';
-import * as stay from './stay';
-import * as user from './user';
+import auth from './auth';
+import reservation from './reservation';
+import room from './room';
+import stay from './stay';
+import user from './user';
 
 export { auth, reservation, room, stay, user };
