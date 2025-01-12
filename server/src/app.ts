d1fb423670b4f752ebbf1c525407c64ca73cc45f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-12 15:20:16 +0100

    feat: add routes to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,18 @@ import express from 'express';
 const app = express();
 import cors from 'cors';
 import 'dotenv/config';
+import * as routes from '@routes';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', routes.auth);
+
+app.use('/reservations', routes.reservation);
+app.use('/rooms', routes.room);
+app.use('/stays', routes.stay);
+app.use('/users', routes.user);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server listening on PORT ${process.env.PORT}`)
