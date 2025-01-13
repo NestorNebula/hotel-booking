b9a574dd55b9b38b97cab0d82e33628c783c5596
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-13 18:10:50 +0100

    feat: add types for db models

--- server/src/utils/types.ts
+++ server/src/utils/types.ts
@@ -18,4 +18,46 @@ interface QueryResponse<Type> {
   error: string | null;
 }
 
-export type { PrismaError, QueryResponse };
+interface User {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+  reservations?: Reservation[];
+}
+
+interface Room {
+  id?: number;
+  name: string;
+  description: string;
+  images: string[];
+  pricePerDay: number;
+  numberPerDay: number;
+  reservations?: Reservation[];
+  stays?: Stay[];
+}
+
+interface Reservation {
+  user?: User;
+  userId: number;
+  room?: Room;
+  roomId: number;
+  date: Date;
+  stay?: Stay;
+  stayId?: number | null;
+}
+
+interface Stay {
+  id?: number;
+  firstDay: Date;
+  lastDay: Date;
+  user?: User;
+  userId: number;
+  room?: Room;
+  roomId: number;
+  reservations?: Reservation[];
+}
+
+export type { PrismaError, QueryResponse, User, Room, Reservation, Stay };
