a2ac5015d61cb3c431ae9f321fbad4544c384337
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-14 11:09:24 +0100

    feat: add functions to populate and empty test DB

--- server/tests/data.ts
+++ server/tests/data.ts
@@ -1,10 +1,18 @@
-import { Reservation, Room, Stay, User } from '@prisma/client';
+import type { Reservation, Room, Stay, User } from '@prisma/client';
+import { query, testingQueries } from '@models/queries';
 import {
   getFakeReservation,
   getFakeRoom,
   getFakeStay,
   getFakeUser,
 } from './faker';
+import type { Model } from '@utils/types';
+
+interface Status {
+  dataPopulated: boolean;
+  attempts: number;
+  error: boolean | null;
+}
 
 const getData: () => {
   reservations: Reservation[];
@@ -27,4 +35,83 @@ const getData: () => {
   return { reservations, rooms, stays, users };
 };
 
-export { getData };
+const populateDataModel = async <Data extends Model>(
+  data: Data[],
+  status: Status,
+  queryCb: (arg: Data) => Promise<void>
+) => {
+  for (let i = 0; i < data.length; i++) {
+    const result = await query(() => queryCb(data[i]));
+    if (result.error) {
+      status.error = true;
+      break;
+    }
+  }
+};
+
+const populateDb = async () => {
+  const status: Status = {
+    dataPopulated: false,
+    attempts: 0,
+    error: null,
+  };
+  while (!status.dataPopulated && status.attempts < 10) {
+    status.attempts += 1;
+    status.error = null;
+    const dbEmptied = await emptyDb();
+    if (!dbEmptied) continue;
+    const data = getData();
+    await populateDataModel(
+      data.users,
+      status,
+      testingQueries.__createTestUser
+    );
+    if (status.error) continue;
+    await populateDataModel(
+      data.rooms,
+      status,
+      testingQueries.__createTestRoom
+    );
+    if (status.error) continue;
+    await populateDataModel(
+      data.reservations,
+      status,
+      testingQueries.__createTestReservation
+    );
+    if (status.error) continue;
+    const stays: (Stay & { reservations: Reservation[] })[] = [];
+    for (let i = 0; i < data.stays.length; i++) {
+      const stay = data.stays[i];
+      const reservations: Reservation[] = [];
+      const currentDate = stay.firstDay;
+      while (currentDate <= stay.lastDay) {
+        reservations.push({
+          userId: stay.userId,
+          roomId: stay.roomId,
+          date: currentDate,
+          stayId: stay.id,
+        });
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      await populateDataModel(
+        reservations,
+        status,
+        testingQueries.__createTestReservation
+      );
+      if (status.error) break;
+      stays.push({ ...stay, reservations });
+    }
+    if (status.error) continue;
+    await populateDataModel(stays, status, testingQueries.__createTestStay);
+    if (status.error) continue;
+    status.dataPopulated = true;
+  }
+  return status.dataPopulated;
+};
+
+const emptyDb = async () => {
+  const result = await query(testingQueries.__emptyTestDb);
+  return !result.error;
+};
+
+export { getData, populateDb, emptyDb };
