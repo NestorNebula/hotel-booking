b41fbeff0bb8bc195a387fbb620e78d15af02140
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-14 11:15:28 +0100

    feat: update tests config

--- server/tests/setup.ts
+++ server/tests/setup.ts
@@ -1,6 +1,17 @@
 import request from 'supertest';
 import express from 'express';
+import { populateDb, emptyDb } from './data';
 const app = express();
+
+beforeAll(async () => {
+  const dbPopulated = await populateDb();
+  if (!dbPopulated) throw new Error('Error when populating DB.');
+});
+
+afterAll(async () => {
+  const dbEmptied = await emptyDb();
+  if (!dbEmptied) throw new Error('Error when emptying DB.');
+});
 
 app.use(express.urlencoded({ extended: false }));
 
