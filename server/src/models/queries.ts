a078f08de5dadc6b9db526d9ca703378c87d982e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-14 08:52:10 +0100

    fix: add unique constraint to user's email

--- server/src/models/queries.ts
+++ server/src/models/queries.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
-import type { PrismaError, QueryResponse } from '@utils/types';
+import type {
+  PrismaError,
+  QueryResponse,
+  Reservation,
+  Room,
+  Stay,
+  User,
+} from '@utils/types';
 
 const isTesting = process.env.NODE_ENV === 'test';
 const dbUrl = isTesting
@@ -29,5 +36,52 @@ async function query<Type>(cb: () => Type): Promise<QueryResponse<Type>> {
     };
   }
 }
+
+// User queries
+
+// Room queries
+
+// Reservation queries
+
+// Stay queries
+
+// Test queries
+const testingQueries = {
+  __createTestUser: async (user: User) => {
+    if (!user.id) throw new Error('Test user needs an id.');
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+        isAdmin: user.isAdmin,
+      },
+    });
+  },
+
+  __createTestRoom: async (room: Room) => {
+    if (!room.id) throw new Error('Test room needs an id.');
+    await prisma.room.create({
+      data: {
+        id: room.id,
+        name: room.name,
+        description: room.description,
+        images: room.images,
+        pricePerDay: room.pricePerDay,
+        numberPerDay: room.numberPerDay,
+      },
+    });
+  },
+
+  /* __createTestReservation: async (reservation: Reservation) => {
+  await prisma.reservation.create({
+    data: {
+      userId: {}
+    }
+  })
+} */
+};
 
 export { query };
