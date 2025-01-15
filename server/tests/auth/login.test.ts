12274231e3cba1c5d722e8bc6bd3590e635ad2e4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-15 17:36:59 +0100

    test: add tests for auth/login route

---
+++ server/tests/auth/login.test.ts
@@ -0,0 +1,30 @@
+import { request, app, data } from 'tests/setup/setup';
+import { auth } from '@routes';
+
+app.use('/', auth);
+
+describe('login', () => {
+  it('returns success after successful login', () => {
+    return request(app)
+      .post('/login')
+      .send({ email: data.users[0].email, password: data.users[0].password })
+      .expect(200)
+      .then((res) => {
+        expect(res.body.success).toBeTruthy();
+      });
+  });
+
+  it('returns 400 when trying to connect with a wrong password', (done) => {
+    request(app)
+      .post('/login')
+      .send({ email: data.users[0].email, password: 'wrong password' })
+      .expect(400, done);
+  });
+
+  it('returns 400 when trying to access non-existent user', (done) => {
+    request(app)
+      .post('/login')
+      .send({ email: 'this@email.com', password: data.users[0].password })
+      .expect(400, done);
+  });
+});
