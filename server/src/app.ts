c659c89744813ea3f4c6ce921cf5cf5cb762fffc
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-15 09:42:33 +0100

    feat: add error handlers middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 import cors from 'cors';
 import 'dotenv/config';
 import * as routes from '@routes';
+import Sperror from 'sperror';
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,16 @@ app.use('/reservations', routes.reservation);
 app.use('/rooms', routes.room);
 app.use('/stays', routes.stay);
 app.use('/users', routes.user);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  err instanceof Sperror
+    ? next(err)
+    : next(new Sperror('Unexpected error.', err.message, 500));
+});
+
+app.use((err: Sperror, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.statusCode).json({ error: err });
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Server listening on PORT ${process.env.PORT}`)
