8c3756c9534513030dc133e73e05b3de46ef0f20
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-15 18:21:14 +0100

    feat: add login data validator to auth/login route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import * as auth from '@controllers/auth';
-import { validateNewUser } from '@utils/validators';
+import { validateNewUser, validateLoginData } from '@utils/validators';
 const router = Router();
 
 router.post('/signup', validateNewUser, auth.signup);
-router.post('/login', auth.login);
+router.post('/login', validateLoginData, auth.login);
 router.get('/guest', auth.guest);
 router.get('/refresh', auth.refresh);
 router.post('/admin', auth.admin);
