db7afb5abc3f0099fcbc540cc20c8ea873f49021
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-15 12:12:13 +0100

    feat: add passport-jwt strategy

---
+++ server/src/utils/passport.ts
@@ -0,0 +1,31 @@
+import passport from 'passport';
+import {
+  ExtractJwt,
+  Strategy,
+  StrategyOptionsWithoutRequest,
+} from 'passport-jwt';
+import { Request } from 'express';
+import { query, getUserById } from '@models/queries';
+import 'dotenv/config';
+
+const cookieExtractor = (req: Request) => {
+  return req.cookies.token;
+};
+
+const options: StrategyOptionsWithoutRequest = {
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: process.env.T!,
+};
+
+passport.use(
+  new Strategy(options, async (data: { id: number }, done) => {
+    const { result: user, error } = await query(() => getUserById(data.id));
+    if (error) {
+      return error.type === 'Unexpected'
+        ? done(error, false)
+        : done(null, false);
+    } else {
+      return done(null, user);
+    }
+  })
+);
