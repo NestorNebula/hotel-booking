c8884b1b82b9e28b7c408890cb40d0865ee4c1a5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-16 13:02:21 +0100

    test: add tests for reservation/post route

---
+++ server/tests/reservation/post.test.ts
@@ -0,0 +1,38 @@
+import { app, data, request } from 'tests/setup/setup';
+import { reservation } from '@routes';
+
+app.use('/', reservation);
+
+describe('post', () => {
+  it('returns reservation after creating it', () => {
+    return request(app)
+      .post('/')
+      .send({ roomId: data.rooms[0].id, date: new Date(Date.now()) })
+      .type('form')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reservation).toBeDefined();
+      });
+  });
+
+  it('lets doing multiple reservations for different users on rooms having more than 1 as numberPerDay', () => {
+    return request(app)
+      .post('/')
+      .send({ roomId: data.rooms[1], date: data.reservations[1].date })
+      .type('form')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reservation.date.getDate()).toBe(
+          data.reservations[1].date
+        );
+      });
+  });
+
+  it('returns 400 when trying to book a room already booked on a specific day', (done) => {
+    request(app)
+      .post('/')
+      .send({ roomId: data.rooms[2], date: data.reservations[2].date })
+      .type('form')
+      .expect(400, done);
+  });
+});
