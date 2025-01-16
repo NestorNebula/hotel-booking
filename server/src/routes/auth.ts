fd1677c46ab48597e0045684a0a5d89378e0a4fa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-16 10:36:21 +0100

    feat: add passport middleware to auth/admin route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 import * as auth from '@controllers/auth';
-import { validateNewUser, validateLoginData } from '@utils/validators';
+import {
+  validateNewUser,
+  validateLoginData,
+  validateAdminPassword,
+} from '@utils/validators';
+import passport from 'passport';
+import '@utils/passport/strategy';
+import { handleError } from '@utils/passport/fail';
 const router = Router();
 
 router.post('/signup', validateNewUser, auth.signup);
 router.post('/login', validateLoginData, auth.login);
 router.get('/guest', auth.guest);
 router.get('/refresh', auth.refresh);
-router.post('/admin', auth.admin);
+router.post(
+  '/admin',
+  passport.authenticate('jwt', { session: false, failWithError: true }),
+  handleError,
+  validateAdminPassword,
+  auth.admin
+);
 
 export default router;
