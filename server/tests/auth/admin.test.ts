81e925d2cfc81a7328d95473b31f17039e24e691
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-16 10:06:25 +0100

    test: use post requests instead of get requests

--- server/tests/auth/admin.test.ts
+++ server/tests/auth/admin.test.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 describe('admin', () => {
   it('returns success when user send the correct password', () => {
     return request(app)
-      .get('/admin')
+      .post('/admin')
       .send({ password: process.env.ADMIN_PWD })
       .type('form')
       .expect(200)
@@ -16,7 +16,7 @@ describe('admin', () => {
 
   it('returns 400 when sending wrong password', (done) => {
     request(app)
-      .get('/admin')
+      .post('/admin')
       .send({ password: 'probably not this' })
       .type('form')
       .expect(400, done);
