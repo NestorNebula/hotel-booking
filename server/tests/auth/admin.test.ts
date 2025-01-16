14aa70539b9991f67ff3eb0dc7665c138dc4a7f0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-16 10:35:54 +0100

    test: add tokens to tests

--- server/tests/auth/admin.test.ts
+++ server/tests/auth/admin.test.ts
@@ -1,11 +1,17 @@
 import { app, data, request } from 'tests/setup/setup';
 import { auth } from '@routes';
+import { getToken } from '@utils/jwt';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
+
+app.use(cookieParser());
+app.use('/', auth);
 
 describe('admin', () => {
   it('returns success when user send the correct password', () => {
     return request(app)
       .post('/admin')
+      .set('Cookie', [`token=${getToken(data.users[0].id, '15m')}`])
       .send({ password: process.env.ADMIN_PWD })
       .type('form')
       .expect(200)
@@ -17,6 +23,7 @@ describe('admin', () => {
   it('returns 400 when sending wrong password', (done) => {
     request(app)
       .post('/admin')
+      .set('Cookie', [`token=${getToken(data.users[0].id, '15m')}`])
       .send({ password: 'probably not this' })
       .type('form')
       .expect(400, done);
