207653acfbd3c57868e0521dd2cf7c545049b202
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-18 09:59:29 +0100

    feat: add get function

--- server/src/controllers/stay.ts
+++ server/src/controllers/stay.ts
@@ -1,4 +1,26 @@
-const get = () => {};
+import { Request } from '@utils/ts/types';
+import { Response, NextFunction } from 'express';
+import { query, getStay } from '@models/queries';
+import Sperror from 'sperror';
+
+const get = async (req: Request, res: Response, next: NextFunction) => {
+  const { result: stay, error } = await query(() =>
+    getStay(Number(req.params.stayId))
+  );
+  if (error) {
+    next(
+      error.type === 'Sperror'
+        ? new Sperror('Stay not found', error.message, 404)
+        : new Sperror('Server error', error.message, 500)
+    );
+    return;
+  }
+  if (stay.userId !== req.user!.id) {
+    next(new Sperror('No access right', "You can't access this data.", 403));
+  } else {
+    res.json({ stay });
+  }
+};
 
 const post = () => {};
 
