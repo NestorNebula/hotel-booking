f10df22448a1a903841e719afaced6201cf4c8b2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-18 13:14:38 +0100

    feat: add validator to stay/post route

--- server/src/routes/stay.ts
+++ server/src/routes/stay.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as stay from '@controllers/stay';
+import { validateStay } from '@utils/validators';
 const router = Router();
 
 router.get('/:stayId', stay.get);
-router.post('/', stay.post);
+router.post('/', validateStay, stay.post);
 router.put('/:stayId', stay.put);
 router.delete('/stayId', stay.remove);
 
