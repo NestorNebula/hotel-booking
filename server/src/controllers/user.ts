3156c519c06c7bf9b301db9a676dd49a6484f650
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-19 11:10:57 +0100

    feat: add get function

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,4 +1,27 @@
-const get = () => {};
+import { Request } from '@utils/ts/types';
+import { Response, NextFunction } from 'express';
+import { query, getUserById } from '@models/queries';
+import Sperror from 'sperror';
+
+const get = async (req: Request, res: Response, next: NextFunction) => {
+  const { result: user, error: getUserByIdError } =
+    req.query.reservations !== undefined
+      ? await query(() => getUserById(Number(req.params.userId), true))
+      : await query(() => getUserById(Number(req.params.userId)));
+  if (getUserByIdError) {
+    next(
+      getUserByIdError.type === 'Sperror'
+        ? new Sperror('User not found', getUserByIdError.message, 404)
+        : new Sperror('Server error', getUserByIdError.message, 500)
+    );
+    return;
+  }
+  if (user.id !== req.user!.id) {
+    next(new Sperror('No access rights', "You can't access this data.", 403));
+    return;
+  }
+  res.json({ user });
+};
 
 const put = () => {};
 
