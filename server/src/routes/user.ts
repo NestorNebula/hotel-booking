9b49a1d5db8f754f1047478eba82e9277653a922
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-19 15:20:16 +0100

    feat: add updateUser validator to user/put route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as user from '@controllers/user';
+import { validateUpdatedUser } from '@utils/validators';
 const router = Router();
 
 router.get('/:userId', user.get);
-router.put('/:userId', user.put);
+router.put('/:userId', validateUpdatedUser, user.put);
 
 export default router;
