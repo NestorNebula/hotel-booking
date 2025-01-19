e245610333068612339421d3035f4d18aedf34db
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-19 11:26:48 +0100

    test: add tests for user/put route

---
+++ server/tests/user/put.test.ts
@@ -0,0 +1,59 @@
+import { app, data, request, setUser } from 'tests/setup/setup';
+import { user } from '@routes';
+
+setUser();
+app.use('/', user);
+
+describe('put', () => {
+  it('returns updated user', () => {
+    return request(app)
+      .put(`/${data.users[0].id}`)
+      .send({ firstName: 'First', lastName: 'Last', email: 'this@email.com' })
+      .type('form')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.user).toBeDefined();
+        expect(res.body.user.firstName).toBe('First');
+        expect(res.body.user.lastName).toBe('Last');
+        expect(res.body.user.email).toBe('this@email.com');
+      });
+  });
+
+  it('returns success after updating password', () => {
+    return request(app)
+      .put(`/${data.users[0].id}?password`)
+      .send({ password: data.users[0].password, newPassword: 'password' })
+      .type('form')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.success).toBeTruthy();
+      });
+  });
+
+  it('returns failure when actual password is wrong', () => {
+    return request(app)
+      .put(`/${data.users[0].id}?password`)
+      .send({ password: 'probably not this', newPassword: 'password' })
+      .type('form')
+      .expect(400)
+      .then((res) => {
+        expect(res.body.error.msg).toMatch(/actual password is incorrect/i);
+      });
+  });
+
+  it("returns 403 when trying to update someone else's data", (done) => {
+    request(app)
+      .put(`/${data.users[1].id}`)
+      .send({ firstName: 'First', lastName: 'Last', email: 'this@email.com' })
+      .type('form')
+      .expect(403, done);
+  });
+
+  it('returns 400 when trying to update non-existent user', (done) => {
+    request(app)
+      .put(`/${data.users[0].id + 1}`)
+      .send({ firstName: 'First', lastName: 'Last', email: 'this@email.com' })
+      .type('form')
+      .expect(400, done);
+  });
+});
