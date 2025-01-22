9b27c06ea80cc22a6aed1cbb4cd3687066d638aa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-22 15:43:52 +0100

    feat: add db types

---
+++ client/src/types/db.ts
@@ -0,0 +1,61 @@
+namespace User {
+  export type WithReservations = User & { reservations: Reservation[] };
+
+  export type WithStays = User & { stays: Stay[] };
+}
+
+namespace Room {
+  export type WithReservations = Room & { reservations: Reservation[] };
+
+  export type WithStays = Room & { stays: Stay[] };
+}
+
+namespace Reservation {
+  export type WithUser = Stay & { user: User };
+
+  export type WithRoom = Stay & { room: Room };
+
+  export type WithStay = Stay & { stayId: number; stay: Stay };
+}
+
+namespace Stay {
+  export type WithUser = Stay & { user: User };
+
+  export type WithRoom = Stay & { room: Room };
+
+  export type WithReservations = Stay & { reservations: Reservation };
+}
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+interface Room {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  pricePerDay: number;
+  numberPerDay: number;
+}
+
+interface Reservation {
+  userId: number;
+  roomId: number;
+  date: Date;
+  stayId: null;
+}
+
+interface Stay {
+  id: number;
+  firstDay: Date;
+  lastDay: Date;
+  userId: number;
+  roomId: number;
+}
+
+export type { User, Room, Reservation, Stay };
