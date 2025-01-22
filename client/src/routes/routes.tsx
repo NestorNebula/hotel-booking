745a03998f0385e5131261222f6d51d431921741
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-22 14:26:52 +0100

    feat: create app's routes

---
+++ client/src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import type { RouteObject } from 'react-router';
+import App from '../App';
+
+const routes: RouteObject[] = [
+  {
+    index: true,
+    element: <App />,
+  },
+];
+
+export default routes;
