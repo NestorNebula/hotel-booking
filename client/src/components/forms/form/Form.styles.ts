2794aee48d170e0c4b68a82b5b35180478e5904e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-23 17:49:24 +0100

    feat: add content to Form component

---
+++ client/src/components/forms/form/Form.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Form = styled.form``;
+
+const SubmitButton = styled.button``;
+
+export { Form, SubmitButton };
--- client/src/components/forms/form/Form.tsx
+++ client/src/components/forms/form/Form.tsx
@@ -1,5 +1,30 @@
-function Form() {
-  return <></>;
+import { ReactElement } from 'react';
+import * as S from './Form.styles';
+
+function Form({
+  submit,
+  isValid,
+  name,
+  children,
+}: {
+  submit: () => Promise<any>;
+  isValid: boolean;
+  name?: string;
+  children: ReactElement;
+}) {
+  return (
+    <S.Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        submit();
+      }}
+    >
+      {children}
+      <S.SubmitButton type={isValid ? 'submit' : 'button'}>
+        {name ?? 'Submit'}
+      </S.SubmitButton>
+    </S.Form>
+  );
 }
 
 export default Form;
