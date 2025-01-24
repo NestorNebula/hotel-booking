4078e2cd1ebb3cbf3216521f05231b242b7e3128
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-24 14:47:08 +0100

    test: fix password query
    Query for exact string instead of RegExp
    The RegExp was selecting password and password confirmation inputs

--- client/src/pages/auth/signup/Signup.test.tsx
+++ client/src/pages/auth/signup/Signup.test.tsx
@@ -1,9 +1,14 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import Signup from './Signup';
 
 beforeEach(() => {
-  render(<Signup />);
+  render(
+    <MemoryRouter>
+      <Signup />
+    </MemoryRouter>
+  );
 });
 
 describe('Signup', () => {
@@ -11,6 +16,6 @@ describe('Signup', () => {
     expect(screen.queryByLabelText(/first name/i)).toBeInTheDocument();
     expect(screen.queryByLabelText(/last name/i)).toBeInTheDocument();
     expect(screen.queryByLabelText(/email/i)).toBeInTheDocument();
-    expect(screen.queryByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.queryByLabelText('password')).toBeInTheDocument();
   });
 });
