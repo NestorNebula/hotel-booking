13a26d07754f8d62941626a41171cef3baf75526
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-24 10:37:29 +0100

    feat: add useInput hook

---
+++ client/src/hooks/useInput.ts
@@ -0,0 +1,53 @@
+import { ChangeEvent, useState } from 'react';
+import type { validationChain } from '@services/validationChains';
+
+export namespace useInput {
+  export type value = string;
+
+  export type updateValue = (e: ChangeEvent<HTMLInputElement>) => void;
+
+  export type validation = {
+    isValid: boolean;
+    message: string;
+  };
+}
+
+export type useInput = (
+  validator: ReturnType<validationChain>,
+  initialValue?: string
+) => {
+  value: useInput.value;
+  updateValue: useInput.updateValue;
+  validation: useInput.validation;
+};
+
+const useInput: useInput = (
+  validator: ReturnType<validationChain>,
+  initialValue?: string
+) => {
+  const [value, setValue] = useState(initialValue ?? '');
+  const [validation, setValidation] = useState<useInput.validation>({
+    isValid: true,
+    message: '',
+  });
+
+  const updateValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const beforeResult = validator.before(e.currentTarget.value);
+    if (beforeResult.errors.length) {
+      setValidation({ isValid: false, message: beforeResult.errors[0] });
+      return;
+    }
+    setValue(beforeResult.value);
+    const afterResult = validator.after(beforeResult.value);
+    if (afterResult.errors.length) {
+      setValidation({ isValid: false, message: afterResult.errors[0] });
+    } else {
+      setValidation({ isValid: true, message: '' });
+    }
+    return;
+  };
+
+  return { value, updateValue, validation };
+};
+
+export default useInput;
