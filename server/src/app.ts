895d9e634526e5a6c3e6fdae3d255a7a0826bbd2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-24 15:58:58 +0100

    feat: add cors parameters

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,12 @@ import passport = require('passport');
 import '@utils/passport/strategy';
 import { handleError } from '@utils/passport/fail';
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
