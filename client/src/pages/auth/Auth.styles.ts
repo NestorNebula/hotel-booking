831ee13b0c3caa2d78e5c2518527b76b6dd870ad
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-24 15:21:15 +0100

    feat: add content to Auth page

---
+++ client/src/pages/auth/Auth.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const Auth = styled.main``;
+const Guest = styled.button``;
+
+export { Auth, Guest };
--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,5 +1,31 @@
+import { useNavigate } from 'react-router';
+import Signup from './signup/Signup';
+import Login from './login/Login';
+import fetchAPI from '@services/api';
+import type { APIResponse } from '#types/fetch';
+import * as S from './Auth.styles';
+
 function Auth() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const submitGuest = async () => {
+    const fetch: APIResponse<{ success: true }> = await fetchAPI({
+      path: 'auth/guest',
+      method: 'get',
+    });
+    if (!fetch.error) {
+      localStorage.setItem('id', '0');
+      navigate('/');
+    }
+  };
+
+  return (
+    <S.Auth>
+      <Signup />
+      <Login />
+      <S.Guest onClick={submitGuest}>Sign in as Guest</S.Guest>
+    </S.Auth>
+  );
 }
 
 export default Auth;
