800aee2360babc43e5ce2a153213ce966b6af19c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-24 15:11:59 +0100

    feat: add content to Login

--- client/src/pages/auth/login/Login.tsx
+++ client/src/pages/auth/login/Login.tsx
@@ -1,5 +1,66 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Form, Input } from '@components/forms';
+import { useInput } from '@hooks';
+import fetchAPI from '@services/api';
+import * as validate from '@services/validationChains';
+import type { APIResponse } from '#types/fetch';
+
 function Login() {
-  return <></>;
+  const {
+    value: email,
+    updateValue: updateEmail,
+    validation: emailValidation,
+  } = useInput(validate.email());
+  const {
+    value: password,
+    updateValue: updatePassword,
+    validation: passwordValidation,
+  } = useInput(validate.password());
+  const validations = [emailValidation, passwordValidation];
+  const isValid = validations.every((v) => v.isValid);
+
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const submitLogin = async () => {
+    const fetch: APIResponse<{ success: true; id: number }> = await fetchAPI({
+      path: 'auth/login',
+      body: { email, password },
+      method: 'post',
+    });
+    if (fetch.error) {
+      setError(
+        fetch.result.errors
+          ? fetch.result.errors[0].msg
+          : fetch.result.error.msg || fetch.result.error.message
+      );
+    } else {
+      localStorage.setItem('id', `${fetch.result.id}`);
+      navigate('/');
+    }
+  };
+
+  return (
+    <Form submit={submitLogin} isValid={isValid} name="Log In">
+      <div>Log In</div>
+      <Input
+        name="email"
+        value={email}
+        updateValue={updateEmail}
+        validation={emailValidation}
+        type="email"
+      />
+      <Input
+        name="password"
+        value={password}
+        updateValue={updatePassword}
+        validation={passwordValidation}
+        type="password"
+      />
+      {error ? <div>{error}</div> : <></>}
+    </Form>
+  );
 }
 
 export default Login;
