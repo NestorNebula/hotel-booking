2a9d473c9ef73933c93dd876befcaa7edc5b98c7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 09:20:38 +0100

    feat: add Auth route and its loader

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,12 +1,18 @@
 import type { RouteObject } from 'react-router';
 import App from '../App';
-import { appLoader } from '@loaders';
+import Auth from '@pages/auth/Auth';
+import { appLoader, authLoader } from '@loaders';
 
 const routes: RouteObject[] = [
   {
     index: true,
     element: <App />,
     loader: appLoader,
+  },
+  {
+    path: 'auth',
+    element: <Auth />,
+    loader: authLoader,
   },
 ];
 
