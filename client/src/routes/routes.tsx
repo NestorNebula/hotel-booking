5cf4327cce3262a1984c93a8b2ac7e6b9c725157
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 17:03:44 +0100

    feat: add Home page to routes

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,13 +1,20 @@
 import type { RouteObject } from 'react-router';
 import App from '../App';
 import Auth from '@pages/auth/Auth';
+import Home from '@pages/app/home/Home';
 import { appLoader, authLoader } from '@loaders';
 
 const routes: RouteObject[] = [
   {
-    index: true,
+    path: '/',
     element: <App />,
     loader: appLoader,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
   {
     path: 'auth',
