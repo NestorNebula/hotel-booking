56bb41562a6e5bad4cbf123f63b38f3b116bfcc6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 09:53:22 +0100

    feat: add content to Navbar

---
+++ client/src/pages/app/navbar/Navbar.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Navbar = styled.nav``;
+const SidebarButton = styled.button``;
+const Logo = styled.img``;
+
+export { Navbar, SidebarButton, Logo };
--- client/src/pages/app/navbar/Navbar.tsx
+++ client/src/pages/app/navbar/Navbar.tsx
@@ -1,5 +1,30 @@
-function Navbar() {
-  return <></>;
+import { icon, menu } from '@assets/icons';
+import * as S from './Navbar.styles';
+
+function Navbar({
+  sidebarOpened,
+  updateSidebarDisplay,
+}: {
+  sidebarOpened: boolean;
+  updateSidebarDisplay: () => void;
+}) {
+  return (
+    <S.Navbar>
+      <ul>
+        <li>
+          <S.SidebarButton
+            aria-label={sidebarOpened ? 'close sidebar' : 'open sidebar'}
+            onClick={updateSidebarDisplay}
+          >
+            <img src={menu} alt="menu" />
+          </S.SidebarButton>
+        </li>
+        <li>
+          <S.Logo src={icon} alt="" />
+        </li>
+      </ul>
+    </S.Navbar>
+  );
 }
 
 export default Navbar;
