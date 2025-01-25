a6ae667fcbd68ea115a1204f3b03ae545366437d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 15:39:51 +0100

    feat: add content to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { Outlet, useLoaderData } from 'react-router';
+import { Context } from '@context';
+import Navbar from '@pages/app/navbar/Navbar';
+import Sidebar from '@pages/app/sidebar/Sidebar';
+import type { GuestUser, LoggedUser } from '@context/default';
+import type { Room } from '#types/db';
+import * as S from './App.styles';
+
 function App() {
-  return <></>;
+  const { rooms, user }: { rooms: Room[]; user: LoggedUser | GuestUser } =
+    useLoaderData();
+  const [displaySidebar, setDisplaySidebar] = useState(false);
+  const updateDisplaySidebar = () => {
+    setDisplaySidebar(!displaySidebar);
+  };
+
+  return (
+    <Context.Provider value={{ rooms, user }}>
+      <S.App>
+        <Navbar
+          sidebarOpened={displaySidebar}
+          updateSidebarDisplay={updateDisplaySidebar}
+        />
+        {displaySidebar && (
+          <Sidebar close={updateDisplaySidebar} rooms={rooms} user={user} />
+        )}
+        <Outlet />
+      </S.App>
+    </Context.Provider>
+  );
 }
 
 export default App;
