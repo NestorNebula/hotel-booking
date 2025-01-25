08c4ecc735456a1846f837ca29d1f1fe9341eaca
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 16:54:48 +0100

    feat: add content to Home page

---
+++ client/src/pages/app/home/Home.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Home = styled.main``;
+const Header = styled.header``;
+const RoomList = styled.section``;
+
+export { Home, Header, RoomList };
--- client/src/pages/app/home/Home.tsx
+++ client/src/pages/app/home/Home.tsx
@@ -1,5 +1,36 @@
+import { useContext } from 'react';
+import { redirect } from 'react-router';
+import { Context } from '@context';
+import RoomButton from './roombutton/RoomButton';
+import * as S from './Home.styles';
+
 function Home() {
-  return <></>;
+  const { rooms } = useContext(Context);
+
+  const onRoomClick = (roomId: number) => {
+    redirect(`/rooms/${roomId}`);
+  };
+
+  return (
+    <S.Home>
+      <S.Header>
+        <div>Welcome to our Hotel!</div>
+        <div>
+          We propose multiple rooms with different styles for you to find the
+          perfect place for your next stay!
+        </div>
+      </S.Header>
+      <S.RoomList>
+        {rooms.map((r) => (
+          <RoomButton
+            key={`roomButton${r.id}`}
+            room={r}
+            onClick={() => onRoomClick(r.id)}
+          />
+        ))}
+      </S.RoomList>
+    </S.Home>
+  );
 }
 
 export default Home;
