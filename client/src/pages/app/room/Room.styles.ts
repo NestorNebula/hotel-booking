96d361e89b17306a357af7ed1ce6dba9f416a940
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 18:05:13 +0100

    feat: add content to Room page

---
+++ client/src/pages/app/room/Room.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Room = styled.main``;
+const Details = styled.div``;
+const Description = styled.div``;
+
+export { Room, Details, Description };
--- client/src/pages/app/room/Room.tsx
+++ client/src/pages/app/room/Room.tsx
@@ -1,5 +1,35 @@
+import { useContext } from 'react';
+import { Navigate, redirect, useParams } from 'react-router';
+import { Context } from '@context';
+import Button from '@components/button/Button';
+import * as S from './Room.styles';
+
 function Room() {
-  return <></>;
+  const { roomId } = useParams();
+  const { rooms } = useContext(Context);
+  const room = rooms.find((r) => r.id === Number(roomId));
+
+  const onReserveClick = () => {
+    redirect(`/reserve/${room!.id}`);
+  };
+
+  return (
+    <>
+      {room ? (
+        <S.Room>
+          <img src={`/images/${room.images[0]}.jpg`} alt="room image" />
+          <S.Details>
+            <div>{room.name}</div>
+            <div>${room.pricePerDay} per night</div>
+            <Button onClick={onReserveClick}>Reserve</Button>
+          </S.Details>
+          <S.Description>{room.description}</S.Description>
+        </S.Room>
+      ) : (
+        <Navigate to={'/'} />
+      )}
+    </>
+  );
 }
 
 export default Room;
