cbf76ee5391df7d08b0409c329424df84211682f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 16:43:38 +0100

    feat: add content to RoomButton

--- client/src/pages/app/home/roombutton/RoomButton.tsx
+++ client/src/pages/app/home/roombutton/RoomButton.tsx
@@ -1,5 +1,17 @@
-function RoomButton() {
-  return <></>;
+import type { Room } from '#types/db';
+import * as S from './RoomButton.styles';
+
+function RoomButton({ room, onClick }: { room: Room; onClick: () => void }) {
+  return (
+    <S.RoomButton
+      aria-label={`access to room "${room.name}" details`}
+      onClick={onClick}
+    >
+      <img src={`images/${room.images[0]}.jpg`} alt="room image" />
+      <div>{room.name}</div>
+      <div>${room.pricePerDay} per night</div>
+    </S.RoomButton>
+  );
 }
 
 export default RoomButton;
