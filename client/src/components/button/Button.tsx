8f9e0168a3bc130297d4032edca97571ca9c9606
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 17:18:22 +0100

    feat: add content to Button component

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -1,5 +1,13 @@
-function Button() {
-  return <></>;
+import * as S from './Button.styles';
+
+function Button({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: string;
+}) {
+  return <S.Button onClick={onClick}>{children}</S.Button>;
 }
 
 export default Button;
