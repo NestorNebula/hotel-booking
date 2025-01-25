2913ce94494e9ba41d38d4f7f4a4be1dd0da7458
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-25 11:30:43 +0100

    feat: add content to Sidebar

---
+++ client/src/pages/app/sidebar/Sidebar.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Sidebar = styled.nav``;
+const Link = styled.a``;
+const RoomList = styled.ul``;
+
+export { Sidebar, Link, RoomList };
--- client/src/pages/app/sidebar/Sidebar.tsx
+++ client/src/pages/app/sidebar/Sidebar.tsx
@@ -1,5 +1,32 @@
-function Sidebar() {
-  return <></>;
+import { Room, User } from '#types/db';
+import * as S from './Sidebar.styles';
+
+function Sidebar({ rooms, user }: { rooms: Room[]; user: User }) {
+  return (
+    <S.Sidebar>
+      <ul>
+        <li>
+          <S.Link href="/">Home</S.Link>
+        </li>
+        <li>Rooms</li>
+        <S.RoomList>
+          {rooms.map((room) => (
+            <S.Link key={`linkto${room.id}`} href={`/rooms/${room.id}`}>
+              {room.name}
+            </S.Link>
+          ))}
+        </S.RoomList>
+        <li>
+          <S.Link href="/account">Your Account</S.Link>
+        </li>
+        <li>
+          <S.Link href="/admin">
+            {user.isAdmin ? 'Admin Dashboard' : 'Become an Admin'}
+          </S.Link>
+        </li>
+      </ul>
+    </S.Sidebar>
+  );
 }
 
 export default Sidebar;
