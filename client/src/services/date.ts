914bfa9ef26cd0171a0a2f8a6c0280983a50a9c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-28 10:10:45 +0100

    feat: add date helpers

---
+++ client/src/services/date.ts
@@ -0,0 +1,68 @@
+import { getISODay } from 'date-fns';
+
+const months: [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+type DateOrNull = Date | null;
+type Week = [
+  DateOrNull,
+  DateOrNull,
+  DateOrNull,
+  DateOrNull,
+  DateOrNull,
+  DateOrNull,
+  DateOrNull
+];
+type MonthWeeks = Week[];
+
+function isWeek(week: unknown): week is Week {
+  return (week as Week).length === 7;
+}
+
+const getMonthWeeks: (date: Date) => MonthWeeks = (date) => {
+  let day = new Date(date.toJSON());
+  day.setUTCDate(1);
+  const monthNumber = day.getMonth();
+  const month: MonthWeeks = [];
+  while (day.getMonth() === monthNumber) {
+    const week: DateOrNull[] = [];
+    for (let i = 0; i < 7; i++) {
+      let weekDay = getISODay(day) - 1;
+      week.push(
+        weekDay === week.length && day.getMonth() === monthNumber
+          ? new Date(day.toJSON())
+          : null
+      );
+      day.setDate(day.getDate() + 1);
+    }
+    isWeek(week) && month.push(week);
+  }
+  return month;
+};
+
+export { months, getMonthWeeks };
