eac7690cb22833ddf560d9a03db3528d71d28e11
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-28 08:44:30 +0100

    test: add tests for Calendar component

---
+++ client/src/components/calendar/Calendar.test.tsx
@@ -0,0 +1,155 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Calendar from './Calendar';
+import { getFakeReservation, getFakeRoom } from '@services/tests/data';
+import { getDaysInMonth } from 'date-fns';
+import type { Reservation } from '#types/db';
+
+const room = getFakeRoom({ multiple: true });
+const reservations: Reservation[] = [];
+const date = new Date();
+date.setUTCHours(0, 0, 0, 0);
+const month = date.toLocaleDateString('en-US', { month: 'long' });
+const year = date.getUTCFullYear();
+
+const mockSetDate = vi.fn();
+
+describe('Calendar', () => {
+  it('renders calendar', () => {
+    render(
+      <Calendar room={room} reservations={reservations} setDate={mockSetDate} />
+    );
+    expect(
+      screen.queryByText(new RegExp(`${month} ${year}`))
+    ).toBeInTheDocument();
+    expect(screen.queryAllByRole('button', { name: /select/i })).toHaveLength(
+      getDaysInMonth(date)
+    );
+  });
+
+  it('renders previous/next month when clicking on corresponding buttons', async () => {
+    render(
+      <Calendar room={room} reservations={reservations} setDate={mockSetDate} />
+    );
+    const nextMonthDate = new Date(date.toJSON());
+    nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
+    const user = userEvent.setup();
+    const nextButton = screen.getByRole('button', { name: /next/i });
+    await user.click(nextButton);
+    expect(
+      screen.queryByText(
+        new RegExp(
+          `${nextMonthDate.toLocaleDateString('en-US', {
+            month: 'long',
+          })} ${nextMonthDate.getUTCFullYear()}`
+        )
+      )
+    ).toBeInTheDocument();
+    const previousButton = screen.getByRole('button', { name: /previous/i });
+    await user.click(previousButton);
+    expect(
+      screen.queryByText(new RegExp(`${month} ${year}`))
+    ).toBeInTheDocument();
+  });
+
+  it('disables all buttons before date', () => {
+    render(
+      <Calendar room={room} reservations={reservations} setDate={mockSetDate} />
+    );
+    let day = date.getUTCDate();
+    while (day > 1) {
+      expect(
+        screen.queryByRole('button', { name: new RegExp(`${day - 1}`) })
+      ).toHaveAttribute('disabled');
+      day = day - 1;
+    }
+  });
+
+  it('disables all buttons before startDate', () => {
+    const startDate = new Date();
+    startDate.setDate(
+      startDate.getDate() > 25
+        ? startDate.getDate() + 10
+        : startDate.getDate() + 2
+    );
+    render(
+      <Calendar
+        room={room}
+        reservations={reservations}
+        setDate={mockSetDate}
+        startDate={startDate}
+      />
+    );
+    let day = date.getUTCDate();
+    while (day > 0) {
+      expect(
+        screen.queryByRole('button', { name: new RegExp(`${day}`) })
+      ).toHaveAttribute('disabled');
+      day = day - 1;
+    }
+  });
+
+  it('disables button on date when reservations are full', async () => {
+    render(
+      <Calendar room={room} reservations={reservations} setDate={mockSetDate} />
+    );
+    const reservationDate = new Date(date.toJSON());
+    reservationDate.setDate(reservationDate.getDate() + 2);
+    reservations.push(
+      getFakeReservation({ roomId: room.id, date: reservationDate }),
+      getFakeReservation({ roomId: room.id, date: reservationDate })
+    );
+    if (reservationDate.getDate() > date.getDate()) {
+      expect(
+        screen.queryByRole('button', {
+          name: new RegExp(`${reservationDate.getDate()}`),
+        })
+      ).toHaveAttribute('disabled');
+    } else {
+      const user = userEvent.setup();
+      await user.click(screen.getByRole('button', { name: /next/i }));
+      expect(
+        screen.queryByRole('button', {
+          name: new RegExp(`${reservationDate.getDate()}`),
+        })
+      ).toHaveAttribute('disabled');
+    }
+  });
+
+  it('disables all buttons after the day reservations are full for selecting endDate', () => {
+    render(
+      <Calendar
+        room={room}
+        reservations={reservations}
+        setDate={mockSetDate}
+        startDate={date}
+      />
+    );
+    const day = new Date(reservations[0].date.getDate());
+    day.setDate(day.getDate() + 1);
+    while (day.getDate() !== reservations[0].date.getDate()) {
+      expect(
+        screen.queryByRole('button', { name: new RegExp(`${day.getDate()}`) })
+      ).toBeDisabled();
+      day.setDate(day.getDate() + 1);
+    }
+  });
+
+  it('calls setDate only on correct date click', async () => {
+    render(
+      <Calendar room={room} reservations={reservations} setDate={mockSetDate} />
+    );
+    const user = userEvent.setup();
+    await user.click(
+      screen.getByRole('button', {
+        name: new RegExp(`${reservations[0].date.getDate()}`),
+      })
+    );
+    expect(mockSetDate).not.toHaveBeenCalled();
+    await user.click(
+      screen.getByRole('button', { name: new RegExp(`${date.getDate()}`) })
+    );
+    expect(mockSetDate).toHaveBeenCalled();
+  });
+});
