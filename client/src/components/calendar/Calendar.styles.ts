9b0a3e993cb8f4cd080c1eb923f187576e60d9df
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-28 14:56:18 +0100

    feat: add content to Calendar component

---
+++ client/src/components/calendar/Calendar.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Calendar = styled.section``;
+const Header = styled.header``;
+const PreviousButton = styled.button``;
+const NextButton = styled.button``;
+const Week = styled.div``;
+const Day = styled.button``;
+const EmptyDay = styled.div``;
+
+export { Calendar, Header, PreviousButton, NextButton, Week, Day, EmptyDay };
--- client/src/components/calendar/Calendar.tsx
+++ client/src/components/calendar/Calendar.tsx
@@ -1,5 +1,96 @@
-function Calendar() {
-  return <></>;
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { getMonthWeeks, months } from '@services/date';
+import type { Reservation, Room } from '#types/db';
+import * as S from './Calendar.styles';
+
+function Calendar({
+  room,
+  reservations,
+  setDate,
+  startDate,
+}: {
+  room: Room;
+  reservations: Reservation[];
+  setDate: (date: Date) => void;
+  startDate?: Date;
+}) {
+  const date = startDate ?? new Date();
+  date.setUTCHours(0, 0, 0, 0);
+  const [calendarDate, setCalendarDate] = useState(new Date(date.toJSON()));
+  const updateMonth = (direction: 'backward' | 'forward') => {
+    const newCalendarDate = new Date(calendarDate.toJSON());
+    newCalendarDate.setUTCDate(1);
+    const month = newCalendarDate.getUTCMonth();
+    newCalendarDate.setUTCMonth(
+      direction === 'backward' ? month - 1 : month + 1
+    );
+    setCalendarDate(newCalendarDate);
+  };
+
+  const month = months[calendarDate.getUTCMonth()];
+  const year = calendarDate.getUTCFullYear();
+
+  const monthWeeks = getMonthWeeks(calendarDate);
+
+  const maxReservationsPerDay = room.numberPerDay;
+
+  const getCalendar = (calendarMonthWeeks: typeof monthWeeks) => {
+    let unavailableDayEncountered = false;
+    return calendarMonthWeeks.map((week, index) => (
+      <S.Week key={`week${index}`}>
+        {week.map((day) => {
+          const stringDate = day && format(day, 'd MMMM y');
+          const dayReservations = !day
+            ? 0
+            : reservations.filter((r) => r.date.getTime() === day.getTime())
+                .length;
+          const isAvailable = !startDate
+            ? day && day >= date && dayReservations < maxReservationsPerDay
+            : day &&
+              day > date &&
+              day > startDate &&
+              !unavailableDayEncountered;
+          if (startDate && dayReservations >= maxReservationsPerDay) {
+            unavailableDayEncountered = true;
+          }
+          return day ? (
+            <S.Day
+              key={`${day.getDate()}${month}${year}`}
+              aria-label={`select ${stringDate} as ${
+                startDate ? 'first day' : 'last day'
+              }.${!isAvailable ? ` This day isn't available.` : ''}`}
+              disabled={!isAvailable}
+              onClick={() => setDate(day)}
+            >
+              {day.getDate()}
+            </S.Day>
+          ) : (
+            <S.EmptyDay key={`emptyDay${Math.round(Math.random() * 10000)}`} />
+          );
+        })}
+      </S.Week>
+    ));
+  };
+
+  return (
+    <S.Calendar>
+      <S.Header>
+        <S.PreviousButton
+          aria-label="previous month"
+          disabled={date.getUTCMonth() === calendarDate.getUTCMonth()}
+          onClick={() => updateMonth('backward')}
+        ></S.PreviousButton>
+        <div>{`${month} ${year}`}</div>
+        <S.NextButton
+          aria-label="next month"
+          disabled={date.getUTCMonth() === calendarDate.getUTCMonth() + 1}
+          onClick={() => updateMonth('forward')}
+        />
+      </S.Header>
+      {getCalendar(monthWeeks)}
+    </S.Calendar>
+  );
 }
 
 export default Calendar;
