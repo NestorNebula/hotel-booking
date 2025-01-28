77dfb3edad888c64a03dc3cafc5466d9384187cf
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-28 15:29:38 +0100

    feat: add content to ReservationOverview component

---
+++ client/src/components/reservation-overview/ReservationOverview.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ReservationOverview = styled.section``;
+const Details = styled.div``;
+const Buttons = styled.div``;
+
+export { ReservationOverview, Details, Buttons };
--- client/src/components/reservation-overview/ReservationOverview.tsx
+++ client/src/components/reservation-overview/ReservationOverview.tsx
@@ -1,5 +1,36 @@
-function ReservationOverview() {
-  return <></>;
+import { differenceInCalendarDays, format } from 'date-fns';
+import type { Room } from '#types/db';
+import * as S from './ReservationOverview.styles';
+
+function ReservationOverview({
+  room,
+  dates,
+  reserve,
+  edit,
+}: {
+  room: Room;
+  dates: { start: Date; end: Date };
+  reserve: () => void;
+  edit: () => void;
+}) {
+  const numberOfNights = differenceInCalendarDays(dates.end, dates.start);
+  return (
+    <S.ReservationOverview>
+      <img src={`/images/${room.images[0]}.jpg`} alt="" />
+      <S.Details>
+        <div>{room.name}</div>
+        <div>
+          {format(dates.start, 'd MMMM y')} - ${format(dates.end, 'd MMMM y')} (
+          {numberOfNights} nights)
+        </div>
+        <div>${room.pricePerDay * numberOfNights}</div>
+      </S.Details>
+      <S.Buttons>
+        <button onClick={edit}>Edit</button>
+        <button onClick={reserve}>Reserve</button>
+      </S.Buttons>
+    </S.ReservationOverview>
+  );
 }
 
 export default ReservationOverview;
