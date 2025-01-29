65ab587e450b3742b48663b16ebc52270eb79f94
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-29 14:37:51 +0100

    test: add MemoryRouter to test

--- client/src/pages/app/account/account-form/AccountForm.test.tsx
+++ client/src/pages/app/account/account-form/AccountForm.test.tsx
@@ -1,11 +1,16 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import AccountForm from './AccountForm';
 import { getFakeUser } from '@services/tests/data';
 
 describe('AccountForm', () => {
   it('renders all fields', () => {
-    render(<AccountForm user={getFakeUser()} />);
+    render(
+      <MemoryRouter>
+        <AccountForm user={getFakeUser()} />
+      </MemoryRouter>
+    );
     expect(screen.queryByLabelText(/first name/i)).toBeInTheDocument();
     expect(screen.queryByLabelText(/last name/i)).toBeInTheDocument();
     expect(screen.queryByLabelText(/email/i)).toBeInTheDocument();
