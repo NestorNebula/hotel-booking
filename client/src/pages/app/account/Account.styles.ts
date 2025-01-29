8224bb403d065ff909b4e19b76c4e01b91facd37
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-29 17:57:55 +0100

    feat: add content to Account page

---
+++ client/src/pages/app/account/Account.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Account = styled.main``;
+const Infos = styled.section``;
+const CloseButton = styled.button``;
+const Reservations = styled.section``;
+
+export { Account, Infos, CloseButton, Reservations };
--- client/src/pages/app/account/Account.tsx
+++ client/src/pages/app/account/Account.tsx
@@ -1,5 +1,49 @@
+import { useContext, useState } from 'react';
+import { Navigate, useLoaderData } from 'react-router';
+import { Context } from '@context';
+import Button from '@components/button/Button';
+import AccountForm from './account-form/AccountForm';
+import PasswordForm from './password-form/PasswordForm';
+import ReservationList from '@components/reservation-list/ReservationList';
+import * as S from './Account.styles';
+
 function Account() {
-  return <></>;
+  const { user } = useContext(Context);
+  const { reservations } = useLoaderData();
+  const [formActive, setFormActive] = useState<'account' | 'password' | null>(
+    null
+  );
+
+  return !user.isGuest ? (
+    <S.Account>
+      <div>Your Account</div>
+      {formActive && (
+        <S.CloseButton onClick={() => setFormActive(null)}>Close</S.CloseButton>
+      )}
+      {!formActive ? (
+        <S.Infos>
+          <div>
+            {user.firstName} {user.lastName}
+          </div>
+          <div>{user.email}</div>
+          <Button onClick={() => setFormActive('account')}>Edit</Button>
+          <Button onClick={() => setFormActive('password')}>
+            Change Password
+          </Button>
+        </S.Infos>
+      ) : formActive === 'account' ? (
+        <AccountForm user={user} />
+      ) : (
+        <PasswordForm user={user} />
+      )}
+      <S.Reservations>
+        <div>Your Reservations</div>
+        <ReservationList reservations={reservations} />
+      </S.Reservations>
+    </S.Account>
+  ) : (
+    <Navigate to="/" />
+  );
 }
 
 export default Account;
