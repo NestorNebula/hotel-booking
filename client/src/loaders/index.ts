46c984994476aed72e94a7fff20db1041ca6c195
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-29 17:58:41 +0100

    feat: export accountLoader

--- client/src/loaders/index.ts
+++ client/src/loaders/index.ts
@@ -1,6 +1,7 @@
+import accountLoader from './accountLoader';
 import appLoader from '@loaders/appLoader';
 import authLoader from '@loaders/authLoader';
 import reserveLoader from './reserveLoader';
 import stayLoader from './stayLoader';
 
-export { appLoader, authLoader, reserveLoader, stayLoader };
+export { accountLoader, appLoader, authLoader, reserveLoader, stayLoader };
