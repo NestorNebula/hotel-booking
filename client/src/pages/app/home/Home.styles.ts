41838ff370b5f85f6a3a22c70fa8480c4b982d53
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 18:23:38 +0100

    feat: add styles to Home

--- client/src/pages/app/home/Home.styles.ts
+++ client/src/pages/app/home/Home.styles.ts
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-const Home = styled.main``;
-const Header = styled.header``;
-const RoomList = styled.section``;
+const Home = styled.main`
+  font-size: var(--fs-m);
+`;
+const Header = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  padding: 1rem 2rem;
+
+  & > div:first-child {
+    font-size: var(--fs-l);
+  }
+`;
+const RoomList = styled.section`
+  padding: 1rem 5rem 3rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-auto-rows: 1fr;
+  gap: 5rem;
+`;
 
 export { Home, Header, RoomList };
