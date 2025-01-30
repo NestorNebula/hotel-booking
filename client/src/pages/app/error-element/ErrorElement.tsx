cecda8ae1ed41d752929f18f99cf28aae674dad3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 11:22:11 +0100

    feat: add ErrorElement page

---
+++ client/src/pages/app/error-element/ErrorElement.tsx
@@ -0,0 +1,43 @@
+import { useNavigate, useRouteError } from 'react-router';
+import Button from '@components/button/Button';
+import * as S from './ErrorElement.styles';
+
+type Error = {
+  error: object;
+  status: number;
+  statusText: string;
+};
+
+function ErrorElement() {
+  const error = useRouteError();
+
+  function isError(err: unknown): err is Error {
+    return (err as Error).error !== undefined;
+  }
+
+  const errorIsError = isError(error);
+  if (errorIsError) {
+    console.error(error.error);
+  }
+
+  const navigate = useNavigate();
+  const returnHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <S.Error>
+      {errorIsError ? (
+        <>
+          <div>{error.status}</div>
+          <div>{error.statusText}</div>
+          <Button onClick={returnHome}>Home</Button>
+        </>
+      ) : (
+        <div>Unexpected Error</div>
+      )}
+    </S.Error>
+  );
+}
+
+export default ErrorElement;
