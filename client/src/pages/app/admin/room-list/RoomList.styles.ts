801dc23ff943a936573644de1f1c5f2016c8d697
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 09:04:56 +0100

    feat: add content to RoomList

---
+++ client/src/pages/app/admin/room-list/RoomList.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const RoomList = styled.section``;
+const Room = styled.div``;
+
+export { RoomList, Room };
--- client/src/pages/app/admin/room-list/RoomList.tsx
+++ client/src/pages/app/admin/room-list/RoomList.tsx
@@ -1,5 +1,27 @@
-function RoomList() {
-  return <></>;
+import Button from '@components/button/Button';
+import * as S from './RoomList.styles';
+import type { Room } from '#types/db';
+
+function RoomList({
+  rooms,
+  buttonTitle,
+  onRoomClick,
+}: {
+  rooms: Room[];
+  buttonTitle: string;
+  onRoomClick: (arg: number) => void;
+}) {
+  return (
+    <S.RoomList>
+      {rooms.map((r) => (
+        <S.Room key={`room${r.id}`}>
+          <img src={r.images[0]} alt="" />
+          <div>{r.name}</div>
+          <Button onClick={() => onRoomClick(r.id)}>{buttonTitle}</Button>
+        </S.Room>
+      ))}
+    </S.RoomList>
+  );
 }
 
 export default RoomList;
