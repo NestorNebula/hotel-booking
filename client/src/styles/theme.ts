a73d87b76bfbb7dac4f02592fe229837bac3e57b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 18:27:34 +0100

    feat: add colors to theme

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,3 +1,12 @@
-const theme = {};
+const theme = {
+  white: '#ffffff',
+  darkRed: '#870101',
+  grey: '#d6d6d6',
+  darkGrey: '#848482',
+  lightBlue: '#7dd1d1',
+  pink: '#f4b5b5',
+  red: '#ED2939',
+  brown: '#886F68',
+};
 
 export default theme;
