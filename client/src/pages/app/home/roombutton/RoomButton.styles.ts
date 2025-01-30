453e29aaf69c72ea23601f0d80e1e7a414799f19
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 18:23:17 +0100

    feat: add styles to RoomButton

--- client/src/pages/app/home/roombutton/RoomButton.styles.ts
+++ client/src/pages/app/home/roombutton/RoomButton.styles.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
-const RoomButton = styled.button``;
+const RoomButton = styled.button`
+  position: relative;
+  top: 0;
+  transition: top 0.25s ease-in;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
+  background-color: ${(props) => props.theme.darkGrey};
+  color: ${(props) => props.theme.white};
+  padding: 2rem;
+  border-radius: 25px;
+  box-shadow: ${(props) => `2px 2px 5px 5px ${props.theme.grey}`};
+
+  &:hover {
+    top: 0.25rem;
+  }
+
+  & > img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 25px;
+  }
+
+  & > div:last-child {
+    font-size: var(--fs-s);
+  }
+`;
 
 export { RoomButton };
--- client/src/pages/app/home/roombutton/RoomButton.tsx
+++ client/src/pages/app/home/roombutton/RoomButton.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import type { Room } from '#types/db';
 import * as S from './RoomButton.styles';
 
 function RoomButton({ room, onClick }: { room: Room; onClick: () => void }) {
+  const [src, setSrc] = useState(`images/${room.images[0]}.jpg`);
+
   return (
     <S.RoomButton
       aria-label={`access to room "${room.name}" details`}
       onClick={onClick}
     >
-      <img src={`images/${room.images[0]}.jpg`} alt="room image" />
+      <img
+        onMouseOver={() => setSrc(`images/${room.images[1]}.jpg`)}
+        onMouseOut={() => setSrc(`images/${room.images[0]}.jpg`)}
+        src={src}
+        alt="room image"
+      />
       <div>{room.name}</div>
       <div>${room.pricePerDay} per night</div>
     </S.RoomButton>
