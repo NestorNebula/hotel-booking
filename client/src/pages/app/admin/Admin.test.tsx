77deacf369f419b4192739d8e8b2bd14886c98ba
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 08:29:28 +0100

    test: add tests for Admin page

---
+++ client/src/pages/app/admin/Admin.test.tsx
@@ -0,0 +1,98 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as React from 'react';
+import { MemoryRouter } from 'react-router';
+import Admin from './Admin';
+import {
+  getFakeReservation,
+  getFakeRoom,
+  getFakeUser,
+} from '@services/tests/data';
+import { Reservation, User } from '#types/db';
+
+const mockUser = getFakeUser();
+const mockAdmin = { ...mockUser, isAdmin: true };
+const mockRooms = [
+  getFakeRoom({ multiple: true }),
+  getFakeRoom({ multiple: false }),
+];
+const mockReservations: (Reservation & { isUser: boolean; user: User })[] = [
+  getFakeReservation({ userId: mockAdmin.id, roomId: mockRooms[0].id }),
+  getFakeReservation({ userId: mockUser.id, roomId: mockRooms[0].id }),
+  getFakeReservation({ userId: mockAdmin.id, roomId: mockRooms[0].id }),
+  getFakeReservation({ userId: mockAdmin.id, roomId: mockRooms[1].id }),
+].map((r) => ({
+  ...r,
+  isUser: r.userId === mockAdmin.id,
+  user: r.userId === mockAdmin.id ? mockAdmin : mockUser,
+}));
+
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <Admin />
+    </MemoryRouter>
+  );
+});
+
+vi.mock('react-router', async () => {
+  const actual = await vi.importActual('react-router');
+  return {
+    ...actual,
+    useLoaderData: () => {
+      return {
+        reservations: mockReservations,
+      };
+    },
+  };
+});
+
+const mockContext = vi.spyOn(React, 'useContext');
+mockContext.mockImplementation(() => {
+  return {
+    user: mockAdmin,
+    rooms: mockRooms,
+  };
+});
+mockContext.mockImplementationOnce(() => {
+  return {
+    user: mockUser,
+    rooms: mockRooms,
+  };
+});
+
+describe('Admin', () => {
+  it("renders admin form when user isn't an admin", () => {
+    expect(screen.queryByLabelText(/admin password/i)).toBeInTheDocument();
+  });
+
+  it('renders all reservations when user is admin', () => {
+    expect(screen.queryAllByText(/reserved/i)).toHaveLength(
+      mockReservations.length
+    );
+  });
+
+  it('displays reservations users names', () => {
+    expect(screen.queryAllByText(/you/i)).toHaveLength(
+      mockReservations.filter((r) => r.isUser).length
+    );
+    expect(
+      screen.queryAllByText(
+        new RegExp(`${mockUser.firstName} ${mockUser.lastName}`)
+      )
+    ).toHaveLength(mockReservations.filter((r) => !r.isUser).length);
+  });
+
+  it('only displays a room reservations after clicking on it', async () => {
+    const user = userEvent.setup();
+    await user.click(
+      screen.getByRole('button', {
+        name: new RegExp(`${mockRooms[0].name} reservations`),
+      })
+    );
+    expect(screen.queryAllByText(/reserved/i)).toHaveLength(
+      mockReservations.filter((r) => r.roomId === mockRooms[0].id).length
+    );
+  });
+});
