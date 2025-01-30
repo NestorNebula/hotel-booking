0322c4ef6bfab923a59397be2c3e1e3af349105c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 08:48:37 +0100

    feat: add content to AdminForm

---
+++ client/src/pages/app/admin/admin-form/AdminForm.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useInput } from '@hooks';
+import { Form, Input } from '@components/forms';
+import fetchAPI from '@services/api';
+import * as validate from '@services/validationChains';
+import type { APIResponse } from '#types/fetch';
+
+function AdminForm() {
+  const {
+    value: adminPassword,
+    updateValue: updateAdminPassword,
+    validation: adminPasswordValidation,
+  } = useInput(validate.adminPassword());
+  const isValid = adminPasswordValidation.isValid;
+
+  const [error, setError] = useState<string | null>(null);
+
+  const submit = async () => {
+    const { result, error }: APIResponse<{ success: boolean }> = await fetchAPI(
+      { path: 'auth/admin', body: { password: adminPassword }, method: 'post' }
+    );
+    if (error || !result.success) {
+      setError(
+        error
+          ? result.errors
+            ? result.errors[0].msg
+            : result.error.msg || result.error.message
+          : 'Error during password processing.'
+      );
+    } else {
+      window.location.reload();
+    }
+  };
+
+  return (
+    <Form submit={submit} isValid={isValid}>
+      <Input
+        name="adminPassword"
+        value={adminPassword}
+        updateValue={updateAdminPassword}
+        validation={adminPasswordValidation}
+        label="Admin Password"
+        type="password"
+      />
+      {error ? <div>{error}</div> : <></>}
+    </Form>
+  );
+}
+
+export default AdminForm;
