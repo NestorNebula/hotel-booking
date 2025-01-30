f637f37abdb6346fd36bead8dcd7073bb0ead753
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-30 17:22:26 +0100

    feat: add styles to Navbar

--- client/src/pages/app/navbar/Navbar.styles.ts
+++ client/src/pages/app/navbar/Navbar.styles.ts
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
-const Navbar = styled.nav``;
-const SidebarButton = styled.button``;
-const Logo = styled.img``;
+const Navbar = styled.nav`
+  padding: 1rem 3rem;
+  background-color: ${(props) => props.theme.brown};
+
+  & > ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const SidebarButton = styled.button`
+  & > img {
+    width: 5rem;
+    filter: invert();
+  }
+`;
+const Logo = styled.img`
+  width: 7.5rem;
+
+  @media (min-width: 800px) {
+    width: 10rem;
+  }
+`;
 
 export { Navbar, SidebarButton, Logo };
