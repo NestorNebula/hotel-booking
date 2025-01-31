346fa5e397d3937e2c211140ae911b6a3ff75dd9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 07:53:39 +0100

    feat: add styles to Button

--- client/src/components/button/Button.styles.ts
+++ client/src/components/button/Button.styles.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${(props) => props.theme.darkRed};
+  color: ${(props) => props.theme.white};
+  padding: 0.75rem 1.5rem;
+  border-radius: 10px;
+  box-shadow: ${(props) => `2px 2px 3px ${props.theme.darkGrey}`};
+  position: relative;
+  top: 0;
+  transition: top 0.5s ease-out, filter 0.25s ease-in;
+
+  &:hover {
+    filter: brightness(125%);
+  }
+
+  &:active {
+    top: 0.25rem;
+  }
+`;
 
 export { Button };
