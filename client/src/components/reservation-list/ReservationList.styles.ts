6313bafb79a11050aabb97217211245c43780a1e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 10:05:29 +0100

    feat: add styles to ReservationList

--- client/src/components/reservation-list/ReservationList.styles.ts
+++ client/src/components/reservation-list/ReservationList.styles.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-const ReservationList = styled.section``;
-const Reservation = styled.div``;
-const Title = styled.div``;
+const ReservationList = styled.section`
+  display: grid;
+  gap: 2rem;
+`;
+const Reservation = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.div`
+  font-size: var(--fs-m);
+  margin-left: 1.5rem;
+`;
 
 export { ReservationList, Reservation, Title };
