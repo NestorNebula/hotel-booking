41635ed5516e71374b1ef8598c81d88f5682f29b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 07:52:44 +0100

    feat: add styles to Room

--- client/src/pages/app/room/Room.styles.ts
+++ client/src/pages/app/room/Room.styles.ts
@@ -1,7 +1,42 @@
 import styled from 'styled-components';
 
-const Room = styled.main``;
-const Details = styled.div``;
-const Description = styled.div``;
+const Room = styled.main`
+  margin: 2.5rem;
+  padding: 2.5rem;
+  background-color: ${(props) => props.theme.grey};
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  & > img {
+    border-radius: 10px;
+  }
+
+  @media (min-width: 800px) {
+    grid-template-columns: 0.75fr 1fr;
+  }
+`;
+const Details = styled.div`
+  font-size: var(--fs-m);
+  place-self: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 2rem;
+
+  & > div:first-child {
+    font-size: var(--fs-l);
+  }
+
+  & > div:nth-of-type(2) {
+    font-size: var(--fs-s);
+  }
+`;
+const Description = styled.div`
+  grid-column: 1 / 3;
+  white-space: pre-wrap;
+  font-size: var(--fs-m);
+  padding-top: 2rem;
+`;
 
 export { Room, Details, Description };
