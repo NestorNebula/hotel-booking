3b8368f0343cb679555b199cb60124613b8b2e44
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 12:39:26 +0100

    feat: add styles to ErrorElement

--- client/src/pages/app/error-element/ErrorElement.styles.ts
+++ client/src/pages/app/error-element/ErrorElement.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-const Error = styled.main``;
+const Error = styled.main`
+  font-size: var(--fs-l);
+  display: grid;
+  place-content: center;
+  text-align: center;
+  min-height: 100vh;
+`;
 
 export { Error };
