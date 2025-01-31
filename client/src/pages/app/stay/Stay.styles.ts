3cd174a1df4912c79a078c8f515dba8d7d7f4665
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 09:23:21 +0100

    feat: add styles to Stay

--- client/src/pages/app/stay/Stay.styles.ts
+++ client/src/pages/app/stay/Stay.styles.ts
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
 
-const Stay = styled.main``;
-const Details = styled.div``;
+const Stay = styled.main`
+  margin: 2.5rem;
+  font-size: var(--fs-l);
+  display: flex;
+  flex-direction: column;
+
+  & > * {
+    padding: 1rem;
+  }
+
+  & > *:not(:last-child) {
+    border-bottom: ${(props) => `1px solid ${props.theme.grey}`};
+  }
+`;
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  text-align: center;
+`;
 
 export { Stay, Details };
