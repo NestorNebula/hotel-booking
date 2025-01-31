97ee509e2e1867659234fab021e3f559d8f336f9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 13:11:24 +0100

    feat: add github icon

--- client/src/assets/icons/index.ts
+++ client/src/assets/icons/index.ts
@@ -1,7 +1,8 @@
 import arrowLeft from './arrow-left.svg';
 import arrowRight from './arrow-right.svg';
 import close from './close.svg';
+import github from './github.svg';
 import icon from './icon.png';
 import menu from './menu.svg';
 
-export { arrowLeft, arrowRight, close, icon, menu };
+export { arrowLeft, arrowRight, close, github, icon, menu };
