7df2bf9464681a5e225bc221b5cf585e29befc8c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-31 11:51:47 +0100

    feat: add Carousel to Room

--- client/src/pages/app/room/Room.styles.ts
+++ client/src/pages/app/room/Room.styles.ts
@@ -15,6 +15,37 @@ const Room = styled.main`
     grid-template-columns: 0.75fr 1fr;
   }
 `;
+const Carousel = styled.div<{ $photoNumber: number }>`
+  position: relative;
+  overflow-x: hidden;
+
+  & > div:first-child {
+    display: flex;
+    position: relative;
+    left: ${(props) => `calc(${props.$photoNumber} * -100%)`};
+    transition: left 0.75s ease-in;
+  }
+`;
+const Points = styled.div`
+  position: absolute;
+  bottom: 0.5rem;
+  left: 45%;
+  display: flex;
+  gap: 0.5rem;
+`;
+const Point = styled.button<{ $isActive: boolean }>`
+  --size: 1rem;
+  height: var(--size);
+  width: var(--size);
+  border-radius: var(--size);
+  background-color: ${(props) =>
+    props.$isActive ? props.theme.pink : props.theme.white};
+  transition: background-color 0.75s ease-in;
+
+  @media (min-width: 800px) {
+    --size: 1.5rem;
+  }
+`;
 const Details = styled.div`
   font-size: var(--fs-m);
   place-self: center;
@@ -39,4 +70,4 @@ const Description = styled.div`
   padding-top: 2rem;
 `;
 
-export { Room, Details, Description };
+export { Room, Carousel, Points, Point, Details, Description };
--- client/src/pages/app/room/Room.tsx
+++ client/src/pages/app/room/Room.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Navigate, useNavigate, useParams } from 'react-router';
 import { Context } from '@context';
 import Button from '@components/button/Button';
@@ -14,11 +14,42 @@ function Room() {
     navigate(`/reserve/${room!.id}`);
   };
 
+  const [activePhoto, setActivePhoto] = useState(0);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setActivePhoto(
+        room && activePhoto < room.images.length - 1 ? activePhoto + 1 : 0
+      );
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  });
+
   return (
     <>
       {room ? (
         <S.Room>
-          <img src={`/images/${room.images[0]}.jpg`} alt="room image" />
+          <S.Carousel $photoNumber={activePhoto}>
+            <div>
+              {room.images.map((i) => (
+                <img
+                  key={`${i}image`}
+                  src={`/images/${i}.jpg`}
+                  alt="room image"
+                />
+              ))}
+            </div>
+            <S.Points>
+              {room.images.map((i, index) => (
+                <S.Point
+                  key={`${i}button`}
+                  aria-label={`display photo ${index + 1}`}
+                  onClick={() => setActivePhoto(index)}
+                  $isActive={index === activePhoto}
+                ></S.Point>
+              ))}
+            </S.Points>
+          </S.Carousel>
           <S.Details>
             <div>{room.name}</div>
             <div>${room.pricePerDay} per night</div>
